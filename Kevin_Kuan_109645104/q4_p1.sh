dcdaf6293d44c9de3f6858c72dc7b9b2fe7a098c
Kevin <kevinkuan4@gmail.com>
2018-04-19 14:15:37 -0400

    q4_p1 start

---
+++ Kevin_Kuan_109645104/q4_p1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Kevin Kuan
+# 109645104
+# CSE337 
+# HW 3
+
+DIR=$1
+
+# Changes to the directory specified from argument
+cd $DIR
+
+echo "Current username is : $USER"
+echo "Current directory is : $PWD"
+echo "Home directory is : $HOME"
+echo
+echo ""
